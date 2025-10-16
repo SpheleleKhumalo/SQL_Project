6ae95a4e570c2a0bfd7747915c41a721a486cff4
Sphelele Khumalo <138694364+SpheleleKhumalo@users.noreply.github.com>
2025-10-16 19:45:44 +0200

    Practise

--- sql/dates.sql
+++ sql/dates.sql
@@ -13,3 +13,26 @@ SELECT
 FROM 
     job_postings_fact
 LIMIT 10;
+
+SELECT
+    job_title_short AS title,
+    job_location AS location,
+    job_posted_date AT TIME ZONE 'UTC' AT TIME ZONE 'Africa/Johannesburg' AS date_time,
+    EXTRACT(MONTH FROM job_posted_date) AS date_month,
+    EXTRACT(YEAR FROM job_posted_date) AS date_year
+FROM 
+    job_postings_fact
+LIMIT 10;
+
+SELECT
+    COUNT(job_id) AS job_count,
+    EXTRACT(MONTH FROM job_posted_date) AS month
+FROM
+    job_postings_fact
+WHERE 
+    job_title_short = 'Data Analyst'
+GROUP BY
+    month 
+ORDER BY
+    job_count DESC;
+
