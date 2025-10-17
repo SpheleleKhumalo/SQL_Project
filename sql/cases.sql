9c93f0cb078cd1a0fc1a063b1194cf4bd8fd3b91
Sphelele Khumalo <138694364+SpheleleKhumalo@users.noreply.github.com>
2025-10-17 19:53:48 +0200

    add code

---
+++ sql/cases.sql
@@ -0,0 +1,31 @@
+CREATE TABLE january_jobs AS
+    SELECT *
+    FROM job_postings_fact
+    WHERE EXTRACT(MONTH FROM job_posted_date) = 1;
+
+CREATE TABLE february_jobs AS
+    SELECT *
+    FROM job_postings_fact
+    WHERE EXTRACT(MONTH FROM job_posted_date) = 2;
+
+CREATE TABLE march_jobs AS
+    SELECT *
+    FROM job_postings_fact
+    WHERE EXTRACT(MONTH FROM job_posted_date) = 3;
+
+SELECT
+    COUNT(job_id) AS number_of_jobs,
+    CASE
+        WHEN job_location = 'Anywhere' THEN 'Remote'
+        WHEN job_location = 'Durban, South Africa' THEN 'Local Durban'
+        WHEN job_location = 'Cape Town, South Africa' THEN 'Local Cape Town'
+        WHEN job_location = 'Johannesburg, South Africa' THEN 'Local Johannesburg'
+        ELSE 'Onsite'
+    END AS location_category
+FROM
+    job_postings_fact
+WHERE
+    job_title_short = 'Data Analyst'
+GROUP BY
+    location_category
+
