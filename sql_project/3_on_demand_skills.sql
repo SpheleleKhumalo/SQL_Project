8f88b356c384127f55c581d5405f2591e012703f
Sphelele Khumalo <138694364+SpheleleKhumalo@users.noreply.github.com>
2025-10-25 12:18:33 +0200

    add code

---
+++ sql_project/3_on_demand_skills.sql
@@ -0,0 +1,25 @@
+/*
+- Note: salary figures are in USD ($).
+*/
+
+/*
+Question: What are the top 5 most in-demand skills for high-paying data roles in Cape Town, Durban, and Johannesburg?
+- Focus on roles titled Data Scientist, Data Analyst, and Data Engineer.
+- Include only jobs located in the three specified cities.
+- Count how many times each skill appears across qualifying job postings.
+- Return the top 5 skills based on demand frequency.
+*/
+
+SELECT 
+    skills,
+    COUNT(skills_job_dim.job_id) AS demand_count
+FROM job_postings_fact
+INNER JOIN skills_job_dim ON job_postings_fact.job_id = skills_job_dim.job_id
+INNER JOIN skills_dim ON skills_job_dim.skill_id = skills_dim.skill_id
+WHERE job_title_short IN('Data Scientist', 'Data Analyst', 'Data Engineer') AND 
+        job_location IN ('Cape Town, South Africa', 'Durban, South Africa', 'Johannesburg, South Africa')  
+GROUP BY 
+    skills
+ORDER BY
+    demand_count DESC
+LIMIT 5
